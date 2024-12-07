7ea752133a06ee18f338c368ad1aa00057eff776
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2024-12-07 11:14:44 +0100

    Update cli.py to import actual package and update code to run

--- preciceconfigchecker/cli.py
+++ preciceconfigchecker/cli.py
@@ -1,18 +1,22 @@
-#from config-graph import buildGraph # TODO: Import adjacent library
-#from pyprecice import Participant
-#
-#if __name__ == "__main__":
-#    # Step 1: Use PreCICE itself to check for basic errors
-#    Participant.check(...)
-#    # Step 2: Detect more issues through the use of a graph
-#    buildGraph("precice-config.xml")
-
+from precice_config_graph import graph, xml_processing
+from pyprecice import Participant
 from collection_of_rules import CollectionOfRules
 
+
 def test_rules() -> None:
-    collection_of_rules:CollectionOfRules = CollectionOfRules()
+    collection_of_rules: CollectionOfRules = CollectionOfRules()
     collection_of_rules.check_all_rules()
     collection_of_rules.print_result()
 
 if __name__ == "__main__":
-    test_rules()
+    path = sys.argv[1]
+
+    print(f"Checking file at {path}")
+
+    # Step 1: Use PreCICE itself to check for basic errors
+    #TODO: Participant.check(...)
+
+    # Step 2: Detect more issues through the use of a graph
+    root = xml_processing.parse_file(path)
+    graph.get_graph(root)
+    test_rule()
