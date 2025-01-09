c72dee3ddb71d7393b942f0e9719977ba9acd7e5
Alexander <alexander00082@gmail.com>
2025-01-09 16:37:03 +0100

    Small changes to the output

--- preciceconfigchecker/cli.py
+++ preciceconfigchecker/cli.py
@@ -15,19 +15,19 @@ path:str = None
 debug:bool = False
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(usage='%(prog)s', description='checks a PreCICE config.xml file for logical errors.')
-    parser.add_argument('src', type=argparse.FileType('r'), help='path of the config.xml source file.')
-    parser.add_argument('-d', '--debug', action='store_true', help='enables debug mode')
+    parser = argparse.ArgumentParser(usage='%(prog)s', description='Checks a PreCICE config.xml file for logical errors.')
+    parser.add_argument('src', type=argparse.FileType('r'), help='Path of the config.xml source file.')
+    parser.add_argument('-d', '--debug', action='store_true', help='Enables debug mode')
     args = parser.parse_args()
 
     if args.debug:
         debug = args.debug
-        print(f"[{Severity.DEBUG.value}]: debug mode enabled")
+        print(f"[{Severity.DEBUG.value}]: Debug mode enabled")
     if args.src.name.endswith('.xml'):
         path = args.src.name
-        print(f"checking file at '{c.dyeing(path, c.cyan)}'")
+        print(f"Checking file at '{c.dyeing(path, c.cyan)}'")
     else:
-        print(f"[{Severity.ERROR.value}]: '{c.dyeing(args.src.name, c.cyan)}' is not a xml file")
+        print(f"[{Severity.ERROR.value}]: '{c.dyeing(args.src.name, c.cyan)}' is not an xml file")
 
     # Step 1: Use preCICE itself to check for basic errors
     # TODO: Participant.check(...)
@@ -42,4 +42,4 @@ if __name__ == "__main__":
 
     # if the user uses severity=debug, then the severity has to be passed here as an argument
     print_all_results(debug)
-    print("All rules checked")
+    print("All rules checked.")
