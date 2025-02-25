26cc6ef34e9b93b7af898e113fe8af4c1ed926e1
Orlando <orlac2700@gmail.com>
2025-02-25 09:30:19 +0100

    Fixed a bug
    And some reformatting 😼

--- preciceconfigchecker/cli.py
+++ preciceconfigchecker/cli.py
@@ -8,12 +8,12 @@ from precice_config_graph import graph, xml_processing
 
 from rules_processing import check_all_rules, print_all_results
 
-
 path: str = None
 debug: bool = False
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(usage='%(prog)s', description='Checks a PreCICE config.xml file for logical errors.')
+    parser = argparse.ArgumentParser(usage='%(prog)s',
+                                     description='Checks a preCICE config.xml file for logical errors.')
     parser.add_argument('src', type=argparse.FileType('r'), help='Path of the config.xml source file.')
     parser.add_argument('-d', '--debug', action='store_true', help='Enables debug mode')
     args = parser.parse_args()
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     root = xml_processing.parse_file(path)
     graph = graph.get_graph(root)
 
-    # individual checks need the graph
+    # Individual checks need the graph
     check_all_rules(graph, debug)
 
     # if the user uses severity=debug, then the severity has to be passed here as an argument
