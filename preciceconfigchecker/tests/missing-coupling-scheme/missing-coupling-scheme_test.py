cc8ccdc80aa596d9214280f46c47f58f3159c279
Orlando <orlac2700@gmail.com>
2025-02-27 05:23:08 +0100

    Update missing-coupling-scheme_test.py
    define custom equals function

--- preciceconfigchecker/tests/missing-coupling-scheme/missing-coupling-scheme_test.py
+++ preciceconfigchecker/tests/missing-coupling-scheme/missing-coupling-scheme_test.py
@@ -8,6 +8,13 @@ from preciceconfigchecker.rule import rules
 
 from preciceconfigchecker.rules.missing_coupling import MissingCouplingSchemeRule as c
 
+
+def equals(a, b):
+    if type(a) != type(b):
+        return False
+    return vars(a) == vars(b)
+
+
 xml = xml_processing.parse_file("precice-config.xml")
 graph = graph.get_graph(xml)
 
@@ -29,5 +36,7 @@ for rule in rules:
 violations_expected_s = sorted(violations_expected, key=lambda obj: type(obj).__name__)
 violations_actual_s = sorted(violations_actual, key=lambda obj: type(obj).__name__)
 
-assert violations_expected_s == violations_actual_s, "There is an error in missing-coupling-scheme test."
+for violation_e, violation_a in zip(violations_expected_s, violations_actual_s):
+    # print(violation_e, violation_a)
+    assert equals(violation_e, violation_a), "There is an error in missing-coupling-scheme test."
 print("Passed missing-coupling-scheme test.")
