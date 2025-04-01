c7482c3de375fb5e9bd676b02234a060f6415929
Orlando <orlac2700@gmail.com>
2025-04-01 23:23:45 +0200

    Update mapping.py
    added violation that checks that parallel participants dont have invalid mapping format

--- preciceconfigchecker/rules/mapping.py
+++ preciceconfigchecker/rules/mapping.py
@@ -1,7 +1,8 @@
 from typing import List
 
 from networkx.classes import Graph
-from precice_config_graph.nodes import ParticipantNode, MeshNode, MappingNode, Direction, MappingConstraint, MappingType
+from precice_config_graph.nodes import ParticipantNode, MeshNode, MappingNode, Direction, MappingConstraint, \
+    MappingType, CouplingSchemeNode, MultiCouplingSchemeNode
 
 from preciceconfigchecker.rule import Rule
 from preciceconfigchecker.severity import Severity
@@ -114,8 +115,68 @@ class MappingRule(Rule):
                     f" {self.direction.value}ing it.",
                     "Otherwise, please remove it to improve readability."]
 
+    class ParallelParticipantsMappingViolation(Violation):
+        """
+            This class handles participant, which run in parallel, having a wrong form of mapping,
+            i.e., are not of the form read-consistent or write-conservative.
+        """
+
+        def __init__(self, participant_parent: ParticipantNode, participant_stranger: ParticipantNode,
+                     mesh_parent: MeshNode, mesh_stranger, direction: Direction, constraint: MappingConstraint):
+            self.participant_parent = participant_parent
+            self.participant_stranger = participant_stranger
+            self.mesh_parent = mesh_parent
+            self.mesh_stranger = mesh_stranger
+            self.direction = direction
+            if self.direction == Direction.READ:
+                # X reads "from" Y
+                self.connecting_word = "from"
+                self.inverse_connector = "to"
+                self.inverse_direction = Direction.WRITE
+            elif self.direction == Direction.WRITE:
+                # X writes "to" Y
+                self.connecting_word = "to"
+                self.inverse_connector = "from"
+                self.inverse_direction = Direction.READ
+            self.constraint = constraint
+
+        def format_explanation(self) -> str:
+            out: str = (f"The participants {self.participant_parent.name} and "
+                        f"{self.participant_stranger.name} are executing in parallel.")
+            out += (f"\n     Their {self.direction.value}-mapping on meshes {self.mesh_parent.name} and {self.mesh_stranger.name} "
+                    f"with constraint=\"{self.constraint.value}\" is is invalid.")
+            out += (
+                "\n     For parallel participants, only mappings of the form read-consistent and write-conservative are "
+                "allowed.")
+            return out
+
+        def format_possible_solutions(self) -> list[str]:
+            out: list[str] = []
+            # One parameter is correct
+            if self.direction == Direction.READ and self.constraint == MappingConstraint.CONSERVATIVE:
+                out += [
+                    f"Consider changing either the direction of the mapping to direction=\"write\" or the constraint"
+                    f"of the mapping to constraint=\"consistent\"."]
+                out += [f"If you change its direction to direction=\"write\", you need to move"
+                        f" the mapping from {self.participant_parent.name} to {self.participant_stranger.name} and "
+                        f"update the exchange=\"...\" tags in their coupling scheme."]
+            # One parameter is correct
+            elif self.direction == Direction.WRITE and self.constraint == MappingConstraint.CONSISTENT:
+                out += [f"Consider changing either the direction of the mapping to direction=\"read\" or the constraint"
+                        f" of the mapping to constraint=\"conservative\"."]
+                out += [f"If you change its direction to direction=\"read\", you need to move"
+                        f" the mapping from {self.participant_parent.name} to {self.participant_stranger.name} and "
+                        f"update the exchange=\"...\" tags in their coupling scheme."]
+            # Both parameters are incorrect
+            else:
+                out += ["Please change the direction of the mapping to direction=\"write\" and the constraint of the "
+                        "mapping to constraint=\"conservative\" or change the direction of the mapping to direction="
+                        "\"read\" and the constraint of the mapping to constraint=\"consistent\"."]
+                out += [f"Please also check that data between {self.participant_parent.name} and "
+                        f"{self.participant_stranger.name} gets exchanged correctly in their coupling scheme."]
+            return out
+
     class MappingDirectionViolation(Violation):
-        name = "Mapping direction."
 
         def __init__(self, participant_parent: ParticipantNode, participant_stranger: ParticipantNode,
                      mesh_parent: MeshNode, mesh_stranger: MeshNode, direction: Direction):
@@ -160,6 +221,8 @@ class MappingRule(Rule):
         violations: list[Violation] = []
 
         mappings: list[MappingNode] = filter_mapping_nodes(graph)
+        parallel_couplings: list[CouplingSchemeNode | MultiCouplingSchemeNode] = filter_parallel_coupling_nodes(graph)
+
         for mapping in mappings:
 
             # Only consider just-in-time mappings here
@@ -207,6 +270,7 @@ class MappingRule(Rule):
             # 'just-in-time' mappings have already been handled
             elif not mapping.just_in_time:
                 direction: Direction = mapping.direction
+                constraint: MappingConstraint = mapping.constraint
                 # Get participants involved in mapping
                 participant_parent: ParticipantNode = mapping.parent_participant
                 if mapping.from_mesh in participant_parent.provide_meshes:
@@ -216,13 +280,10 @@ class MappingRule(Rule):
                     mesh_parent: MeshNode = mapping.to_mesh
                     mesh_stranger: MeshNode = mapping.from_mesh
                 else:
-                    # TODO A violation: One mesh needs to be by parent.
-                    #  continue, to exit current loop iteration
-
+                    # This should not happen if precice-tools --check ran.
                     pass
 
                 participant_stranger: ParticipantNode = get_participant_of_mesh(graph, mesh_stranger)
-                # TODO: Error if stranger = parent? Mappings should probably not be on the same participant
                 if direction == Direction.WRITE:
                     # If the direction is 'write', then the 'from' mesh needs to be provided by Parent
                     if mapping.from_mesh == mesh_parent:
@@ -232,6 +293,30 @@ class MappingRule(Rule):
                         # Parent is trying to write _to_ Strangers mesh
                         violations.append(self.MappingDirectionViolation(participant_parent, participant_stranger,
                                                                          mesh_parent, mesh_stranger, direction))
+                    # For parallel participants, only a write-conservative mapping is allowed
+                    if mapping.constraint != MappingConstraint.CONSERVATIVE:
+                        for coupling_scheme in parallel_couplings:
+                            if isinstance(coupling_scheme, CouplingSchemeNode):
+                                # Find coupling-scheme corresponding to mapping
+                                if ((coupling_scheme.first_participant == participant_parent and
+                                     coupling_scheme.second_participant == participant_stranger) or
+                                        (coupling_scheme.first_participant == participant_stranger and
+                                         coupling_scheme.second_participant == participant_stranger)):
+                                    violations.append(self.ParallelParticipantsMappingViolation(participant_parent,
+                                                                                                participant_stranger,
+                                                                                                mesh_parent,
+                                                                                                mesh_stranger,
+                                                                                                direction, constraint))
+                            elif isinstance(coupling_scheme, MultiCouplingSchemeNode):
+                                # Find multi-coupling-scheme corresponding to mapping
+                                if (participant_parent in coupling_scheme.participants and
+                                        participant_stranger in coupling_scheme.participants):
+                                    violations.append(self.ParallelParticipantsMappingViolation(participant_parent,
+                                                                                                participant_stranger,
+                                                                                                mesh_parent,
+                                                                                                mesh_stranger,
+                                                                                                direction, constraint))
+
                 elif direction == Direction.READ:
                     # If the direction is 'read', then the 'to' mesh needs to be by Parent
                     if mapping.to_mesh == mesh_parent:
@@ -241,6 +326,30 @@ class MappingRule(Rule):
                         # Parent is trying to read _from_ Stranger
                         violations.append(self.MappingDirectionViolation(participant_parent, participant_stranger,
                                                                          mesh_parent, mesh_stranger, direction))
+                    # For parallel participants, only a read-consistent mapping is allowed.
+                    if mapping.constraint != MappingConstraint.CONSISTENT:
+                        for coupling_scheme in parallel_couplings:
+                            if isinstance(coupling_scheme, CouplingSchemeNode):
+                                # Find coupling-scheme corresponding to mapping
+                                if ((coupling_scheme.first_participant == participant_parent and
+                                     coupling_scheme.second_participant == participant_stranger) or
+                                        (coupling_scheme.first_participant == participant_stranger and
+                                         coupling_scheme.second_participant == participant_stranger)):
+                                    violations.append(self.ParallelParticipantsMappingViolation(participant_parent,
+                                                                                                participant_stranger,
+                                                                                                mesh_parent,
+                                                                                                mesh_stranger,
+                                                                                                direction, constraint))
+                            elif isinstance(coupling_scheme, MultiCouplingSchemeNode):
+                                # Find multi-coupling-scheme corresponding to mapping
+                                if (participant_parent in coupling_scheme.participants and
+                                        participant_stranger in coupling_scheme.participants):
+                                    violations.append(self.ParallelParticipantsMappingViolation(participant_parent,
+                                                                                                participant_stranger,
+                                                                                                mesh_parent,
+                                                                                                mesh_stranger,
+                                                                                                direction, constraint))
+
 
         return violations
 
@@ -278,3 +387,16 @@ def filter_mapping_nodes(graph: Graph) -> list[MappingNode]:
         if isinstance(node, MappingNode):
             mappings.append(node)
     return mappings
+
+
+def filter_parallel_coupling_nodes(graph: Graph) -> list[CouplingSchemeNode | MultiCouplingSchemeNode]:
+    """
+        This function returns all parallel coupling-scheme nodes of the given graph.
+        :param graph:The graph to check.
+        :return: All mapping nodes of the graph, which are either of type multi or ...-parallel.
+    """
+    couplings: list[CouplingSchemeNode | MultiCouplingSchemeNode] = []
+    for node in graph.nodes:
+        if isinstance(node, CouplingSchemeNode) or isinstance(node, MultiCouplingSchemeNode):
+            couplings.append(node)
+    return couplings
