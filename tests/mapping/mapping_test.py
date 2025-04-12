2f541e8f9c5840ac3c9d9e586e1f408c2d4863da
Orlando <orlac2700@gmail.com>
2025-04-12 17:50:03 +0200

    Update mapping_test.py
    adjust mapping test

--- tests/mapping/mapping_test.py
+++ tests/mapping/mapping_test.py
@@ -3,6 +3,8 @@ from precice_config_graph.nodes import ParticipantNode, MeshNode, Direction, Map
 from preciceconfigchecker.rules.mapping import MappingRule as m
 from preciceconfigchecker.rules.m2n_exchange import M2NExchangeRule as mn
 from preciceconfigchecker.rules.data_use_read_write import DataUseReadWriteRule as d
+from preciceconfigchecker.rules_processing import check_all_rules, print_all_results
+from preciceconfigchecker.rules.coupling_scheme_mapping import CouplingSchemeMappingRule as csm
 
 from tests.test_utils import assert_equal_violations, get_actual_violations, create_graph
 
@@ -88,7 +90,11 @@ def test_mapping():
 
         d.DataNotExchangedViolation(d_color, p_generator, p_alligator),
 
-        d.DataNotExchangedViolation(d_color, p_instigator, p_elevator)
+        d.DataNotExchangedViolation(d_color, p_instigator, p_elevator),
+
+        csm.MissingMappingCouplingSchemeViolation(p_generator, p_propagator, m_generator),
+
+        csm.MissingMappingCouplingSchemeViolation(p_incinerator, p_propagator, m_incinerator)
     ]
 
     assert_equal_violations("Mapping-test", violations_expected, violations_actual)
